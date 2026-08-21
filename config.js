120565a9f09ba7a64219dd397c13863919ee19a5
vashyang1120 <vashyang1120@gmail.com>
2026-08-21 18:26:06 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,4 @@
 window.MOSAIC_MAGIC_CONFIG = {
   SEARCH_API_URL: "https://mosaic-magic-search-api.vashyang1120.workers.dev",
-  RESULT_COUNT: 50
+  MAGICIAN_POLL_MS: 1000
 };
-
-
-window.MOSAIC_MAGIC_CONFIG = Object.assign({
-  DEMO_QUERY: '周杰倫',
-  DEMO_CHIPS: ['演唱會', '專輯', '電影', '近照']
-}, window.MOSAIC_MAGIC_CONFIG || {});
